300874c83ec3555dd1483c63e8e10ace3055ec92
Lars Thoms <lars.thoms@uni-hamburg.de>
2023-05-11 12:05:46 +0200

    🚑️ fixed wrong stack version pattern

--- models/job.go
+++ models/job.go
@@ -9,36 +9,16 @@ package models
 
 import (
 	"bytes"
-	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/validator/v10"
-	"github.com/hashicorp/go-version"
-	"regexp"
 )
-
-var versionRegex = regexp.MustCompile(version.VersionRegexpRaw)
 
 type JobRequestQuery struct {
 	Input       string `form:"input" binding:"required"`
 	Timeout     int    `form:"timeout" binding:"omitempty"`
 	PlotURLBase string `form:"ploturlbase" binding:"omitempty"`
-	Version     string `form:"version" binding:"omitempty,version"`
+	Version     string `form:"version" binding:"omitempty"`
 }
 
 type JobResponse struct {
 	Output *bytes.Buffer
 	IsZIP  bool
-}
-
-func init() {
-	versionValidator := func(fl validator.FieldLevel) bool {
-		return versionRegex.MatchString(fl.Field().String())
-	}
-
-	if validate, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		if err := validate.RegisterValidation("version", versionValidator); err != nil {
-			panic("could not register validator")
-		}
-	} else {
-		panic("could not handle validator engine")
-	}
 }
